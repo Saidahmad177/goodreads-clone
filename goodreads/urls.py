4590bc3741ec64bb0558cf83d325a0843c9f8b8c
Saidahmad177 <saidahmad177@mail.ru>
2023-04-24 21:03:31 +0500

    media

--- goodreads/urls.py
+++ goodreads/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ]
-urlpatterns += re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 
+urlpatterns += re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
